cfabff6af8a740aab59235bd1e63440669eede45
Saba <sabanafchigar@gmail.com>
2023-01-22 19:37:17 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var firstName = "Saba"
+var lastName = "Nafchi Gar"
+var age = 31
+var email = "sabanafchigar@gmail.com"
+
+console.log(firstName);
+console.log(typeof firstName);
+console.log(lastName);
+console.log(typeof lastName);
+console.log(age);
+console.log(typeof age);
+console.log(email);
+console.log(typeof email);
+
+console.log("my name is " + firstName + " and my family is " + lastName + "\nand my age is " + age + "\tand my email is " + email);
+
+document.write(`<h2> my name is <span class='lightGreen'>${firstName}</span> and my family is <span class='lightGreen'> ${lastName}</span> and my age is <span class='lightGreen'> ${age}</span> and my email is <span class='lightGreen'> ${email}</span></h2>`)
+
