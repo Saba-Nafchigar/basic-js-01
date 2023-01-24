b5b4522418b2dbf83a8a7011e115fcb38bbe8dc0
Saba <sabanafchigar@gmail.com>
2023-01-24 14:29:21 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+let x = +prompt("enter a number1...")
+console.log(x);
+console.log(typeof x);
+//120
+//number
+
+let y = parseInt(prompt("enter a number2..."))
+console.log(y);
+console.log(typeof y);
+//150
+//number
+
+let z = parseFloat(prompt("enter a number3..."));
+console.log(z);
+console.log(typeof z);
+//12.3
+//number
+
+let w = Number(prompt("anter a number4..."))
+console.log(w);
+console.log(typeof w);
+//12.5 or 12
+//number
