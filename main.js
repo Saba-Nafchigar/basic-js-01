af4c36fd8252c872783d8ea64ab6db8a7f9ac497
Saba <sabanafchigar@gmail.com>
2023-01-24 14:04:18 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let firstName = prompt("Please enter your name" , "saba")
+let lastName = prompt("please enter your lastname")
+let age = prompt("please enter your age")
+let email = prompt("please enter your email")
+
+document.write(`<h1>your name is ${firstName} and your lastname is ${lastName} and your age is ${age}
+ and your email is ${email}</h1>`)
